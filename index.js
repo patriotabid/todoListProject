9d7c9195c72dfca1ed1b9e607019d5adf60507ef
patriotabid <patriot.developer.ca@gmail.com>
2024-08-28 02:58:50 +0400

    added functions: update, checkInput, localStorage and addEventListener for todo form

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
 "use strict";
 
+// Elements variable:
 const formEl = document.querySelector(".container--form");
 const inputEl = document.querySelector(".todo-input");
+const todoListEl = document.querySelector(".todo-list");
 
+// VARIABLE:
+let dataList = [];
+let message = "Please write something";
+
+// FUNCTIONS:
 inputEl.focus();
 
+const checkInput = (input) => (input.value.trim() === "" ? false : true);
+
+const update = function (data, el) {
+  el.textContent = "";
+
+  data.forEach(function (todo) {
+    const todoItemStr = `
+  <li class="todo-item">
+    <div class="todo-item--title">
+      <span class="title--check-icon"><i>icon</i> </span>
+      <p class="title--text">${todo.title}</p>
+    </div>
+
+    <div class="todo-item--icon"></div>
+    </li>
+  `;
+    el.insertAdjacentHTML("afterbegin", todoItemStr);
+  });
+};
+
+// Update DataList:
+dataList = JSON.parse(localStorage.getItem("data"));
+if (!dataList) dataList = [];
+update(dataList, todoListEl);
+
+// MAIN JS:
 formEl.addEventListener("submit", function (e) {
   e.preventDefault();
-  console.log(inputEl.value);
+  if (!checkInput(inputEl)) return alert(message);
+
+  //   Push data new todo item
+  const newTodoItem = { title: inputEl.value };
+  dataList.push(newTodoItem);
+
+  //   Update
+  update(dataList, todoListEl);
+
+  //   Local storage
+  localStorage.setItem("data", JSON.stringify(dataList));
+
+  //   Init function
+  inputEl.value = "";
 });
+``;
