c27b128db4afed00b56bb06efdf3623dbc45a4de
patriotabid <patriot.developer.ca@gmail.com>
2024-08-30 02:27:27 +0400

    added header and completed project!

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const listCountEl = document.querySelector(".list-count");
 const containerTaskbarEl = document.querySelector(".container--taskbar");
 
 // VARIABLE: ............................................................................................................
-let dataList = [];
+let todoItemId,
+  dataList = [];
 let message = "Please write something";
 
 // FUNCTIONS: ............................................................................................................
@@ -16,31 +17,6 @@ let message = "Please write something";
 const checkInput = (input) => (input.value.trim() === "" ? false : true);
 
 const displayListCount = (data) => (listCountEl.textContent = data.length);
-
-const update = function (data) {
-  todoListEl.textContent = "";
-
-  data.forEach(function (todo, i) {
-    const todoItemStr = `
-    <li class="todo-item" id="${i}">
-      <div class="todo-item--title">
-         <img class="title--checkbox ${
-           todo.clicked ? "display--checkbox" : ""
-         }" 
-            src="img/icons8-checkmark-32.png"/>
-         <p class="title--text ${todo.clicked ? "clickedItem" : ""}">
-            ${todo.title}</p>
-      </div>
-    
-       <div class="todo-item--icon"></div>
-    </li>
-    `;
-    todoListEl.insertAdjacentHTML("afterbegin", todoItemStr);
-  });
-
-  // Calculation todo item count
-  displayListCount(data);
-};
 
 const addActivedClass = function (clsname) {
   if (!document.querySelector(`.${clsname}`)) return;
@@ -52,6 +28,32 @@ const addActivedClass = function (clsname) {
     .classList.remove("activited");
 
   document.querySelector(`.${clsname}`).classList.add("activited");
+};
+
+const update = function (data) {
+  todoListEl.textContent = "";
+
+  data.forEach(function (todo) {
+    const todoItemStr = `
+    <li class="todo-item" id="${todo.id}">
+      <div class="todo-item--title">
+         <img class="title--checkbox ${
+           todo.clicked ? "display--checkbox" : ""
+         }" 
+            src="img/icons8-checkmark-32.png"/>
+         <p class="title--text ${todo.clicked ? "clickedItem" : ""}">${
+      todo.title
+    }</p>
+      </div>
+    
+       <div class="todo-item--icon"></div>
+    </li>
+    `;
+    todoListEl.insertAdjacentHTML("afterbegin", todoItemStr);
+  });
+
+  // Calculation todo item count
+  displayListCount(data);
 };
 
 // GETTING STARTED with the SITE ..........................................................................................
@@ -68,8 +70,12 @@ formEl.addEventListener("submit", function (e) {
   e.preventDefault();
   if (!checkInput(inputEl)) return alert(message);
 
+  // Created new id
+  todoItemId = dataList.length ? dataList.length : 0;
+  todoItemId += 1;
+
   //   Push data new todo item
-  const newTodoItem = { title: inputEl.value, clicked: false };
+  const newTodoItem = { title: inputEl.value, clicked: false, id: todoItemId };
   dataList.push(newTodoItem);
 
   //   Update
@@ -84,13 +90,17 @@ formEl.addEventListener("submit", function (e) {
 
 // TODO LIST COMPLETE ITEM
 todoListEl.addEventListener("click", function (e) {
-  const todoItem = e.target.closest(".todo-item");
+  const todoItemEl = e.target.closest(".todo-item");
 
-  if (todoItem) {
-    const index = Number(todoItem.id);
-    dataList[index].clicked = !dataList[index].clicked;
+  if (todoItemEl) {
+    const clickedTodoItem = dataList.find(
+      (todo) => todo.id === Number(todoItemEl.id)
+    );
+    clickedTodoItem.clicked = !clickedTodoItem.clicked;
 
     update(dataList);
+    addActivedClass("list-display--all");
+
     //   Local storage
     localStorage.setItem("data", JSON.stringify(dataList));
   }
@@ -98,29 +108,46 @@ todoListEl.addEventListener("click", function (e) {
 
 // TASKBAR BUTTONS
 containerTaskbarEl.addEventListener("click", function (e) {
-  let newList = dataList;
+  let newList;
+  let acticeClass = e.target.classList.value.slice(4);
 
-  if (e.target.closest(".list-display--all")) newList = dataList;
-
-  if (e.target.closest(".list-display--active"))
+  if (e.target.closest(".list-display--all")) {
+    newList = dataList;
+    addActivedClass(acticeClass);
+    update(newList);
+  }
+  if (e.target.closest(".list-display--active")) {
     newList = dataList.filter((todo) => !todo.clicked);
+    addActivedClass(acticeClass);
+    update(newList);
+  }
 
-  if (e.target.closest(".list-display--completed"))
+  if (e.target.closest(".list-display--completed")) {
     newList = dataList.filter((todo) => todo.clicked);
+    addActivedClass(acticeClass);
+    update(newList);
+  }
 
   if (e.target.closest(".list-delete--completed")) {
     newList = dataList.filter((todo) => !todo.clicked);
     dataList = newList;
+    addActivedClass(acticeClass);
+    update(newList);
     window.location.reload();
     localStorage.setItem("data", JSON.stringify(dataList));
   }
 
   if (e.target.closest(".list-delete--all")) {
     newList = dataList = [];
+    addActivedClass(acticeClass);
+    update(newList);
     localStorage.removeItem("data");
     window.location.reload();
   }
-
-  addActivedClass(e.target.classList.value.slice(4));
-  update(newList);
 });
+
+// Things to do:
+/*
+   6. find header picture for container
+
+ */
